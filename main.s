2834ec789ab1d67b08c9cc480e226fae4d0b9a23
gmestanley <gmestanley@disroot.org>
2025-11-17 18:52:19 -0300

    Better indenting on comments in main

--- main.s
+++ main.s
@@ -1,20 +1,20 @@
-sei ; Set interrupt disable flag
-cld ; Clear decimal mode (2A03 does not have it due to MOS patent) flag
-ldx #$FF	; Load 255 ($FF) into X register
-txs			; Transfer X to stack pointer
-lda #$00	; Load 0 ($00) into Accumulator
-sta $2000	; Store Accumulator (same as Load but doesn't set flags) into $2000
-sta $2001	; Store Accumulator into $2001
+sei             ; Set interrupt disable flag
+cld             ; Clear decimal mode (2A03 does not have it due to MOS patent) flag
+ldx #$FF	    ; Load 255 ($FF) into X register
+txs			    ; Transfer X to stack pointer
+lda #$00	    ; Load 0 ($00) into Accumulator
+sta $2000	    ; Store Accumulator (same as Load but doesn't set flags) into $2000
+sta $2001	    ; Store Accumulator into $2001
 BRANCH:
-lda $2002	; Load Accumulator into $2002
-bpl BRANCH	; Branch back to BRANCH if negative flag clear
-ldy #$02	; Load 2 ($02) into Y register
-sty $01		; Store Y register into $01
-ldy #$00	; Load 0 ($00) into Y register
-tya			; Transfer Y register to Accumulator
+lda $2002	    ; Load Accumulator into $2002
+bpl BRANCH	    ; Branch back to BRANCH if negative flag clear
+ldy #$02	    ; Load 2 ($02) into Y register
+sty $01		    ; Store Y register into $01
+ldy #$00	    ; Load 0 ($00) into Y register
+tya			    ; Transfer Y register to Accumulator
 RAMACCSTORE:
-sta ($00), y; Store accumulator into CPURAM $0000
-iny			; Increment Y register
+sta ($00), y    ; Store accumulator into CPURAM $0000
+iny			    ; Increment Y register
 bne RAMACCSTORE	; Branch to RAMACCSTORE if zero flag clear
 dec $01			; Decrement CPURAM $0001
 bpl RAMACCSTORE	; Branch to RAMACCSTORE if positive (negative flag clear)
