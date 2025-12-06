8f1c096ac8d41c0c5e372ca011e19df2cecafcca
gmestanley <gmestanley@disroot.org>
2025-12-06 13:14:10 -0300

    Added comments to current other instructions in root disasm

--- main.s
+++ main.s
@@ -22,27 +22,30 @@ RAMACCSTORE:
 sta ($00), y	; Store accumulator into CPURAM $0000
 iny				; Increment Y register
 ; A=00 P=?? X=FF Y=01 SP=FF
-bne RAMACCSTORE	; Branch to RAMACCSTORE if zero flag clear
+bne RAMACCSTORE	; Branch to RAMACCSTORE if not equal
 dec $01			; Decrement CPURAM $0001
 bpl RAMACCSTORE	; Branch to RAMACCSTORE if positive (negative flag clear)
 ; A=00 P=?? X=FF Y=01 SP=FF
 lda #$00		; Load 0 ($00) into Accumulator
-sta $10         ; Store Accumulator into $0010
+sta $10			; Store Accumulator into $0010
 ; A=00 P=?? X=FF Y=01 SP=FF
 lda #$00		; Load 0 ($00) into Accumulator
-sta $8000       ; Store Accumulator into $8000
-lda #$1a
-sta $8001
-lda #$20
-sta $2006
-lda #$00
-sta $2006
-lda #$00
-ldy #$00
-ldx #$03
-sta $2007
+sta $8000		; Store Accumulator into $8000
+lda #$1a		; Load 26 ($1A) into Accumulator
+sta $8001		; Store Accumulator into $8001
+lda #$20		; Load 32 ($20) into Accumulator
+sta $2006		; Store Accumulator into $2006
+lda #$00		; Load 0 ($00) into Accumulator
+sta $2006		; Store Accumulator into $2006
+lda #$00		; Load 0 ($00) into Accumulator
+ldy #$00		; Load 0 ($00) into Y register
+ldx #$03		; Load 3 ($03) into X register
+sta $2007		; Store Accumulator into $8000
+LOOP:
 dey				; Decrement Y register
-bne $8052
-ldx #$81
-ldy #$76
+bne LOOP		; Branch if not equal to LOOP
+dex				; Decrement X register
+ldx #$81		; Load 0 ($00) into X register
+ldy #$76		; Load 0 ($00) into Y register
 jsr $80c1
+lda #$00		; Load 0 ($00) into Accumulator
